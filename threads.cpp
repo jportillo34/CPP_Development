d6187eacca801d8e5b718e9b21857a2cef8ee7a9
Jose Portillo <37955235+jportillo34@users.noreply.github.com>
2024-09-29 11:33:15 +0200

    Add files via upload

--- threads.cpp
+++ threads.cpp
@@ -1,21 +1,39 @@
 #include <iostream>
 #include <thread>
 #include <unistd.h>
-#include <vector>
+//#include <vector>
 
-using namespace std;
+double n_global = 1;
 
-double funcOne(double n) { sleep(8); return n; }
+void delayFunction(double n) { sleep(8); n_global += n; }
 
 int main() {
+/*
+  delayFunction(2);
+  delayFunction(2);
+*/
+// OJO INVESTIGAR ESTO!! ASI APARECE EN EL LAB DE LA CHICAGO UNIV Y CREO RECORDAR QUE ASI ERA EN LA ENTREVISTA!!!
+  std::thread t1(&delayFunction, 2);
+  std::thread t2(&delayFunction, 2);
+  t1.join();
+  t2.join();
+
+
+  std::cout << "\n\nn_global = " << n_global << "\n";
+
+
+/*
   std::vector<std::thread> threads;
 
   for (double i = 0; i < 2; i++) {
-    threads.push_back(std::thread(funcOne, i));
-    // funcOne(i);
+    threads.push_back(std::thread(delayFunction, i));
+    //delayFunction(i);
   }
+
   for (auto &t : threads) {
     t.join();
   }
+*/
+
   return (0);
 }
