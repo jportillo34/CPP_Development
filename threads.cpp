7f2ab1164b9a2262159db3411530b96bce13a343
Jose Portillo <37955235+jportillo34@users.noreply.github.com>
2024-09-30 21:59:28 +0200

    Add files via upload

--- threads.cpp
+++ threads.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <thread>
 #include <unistd.h>
+#include <mutex>
 //#include <vector>
 
-double n_global = 1;
+std::mutex mtx;
 
-void delayFunction(double n) { sleep(8); n_global += n; }
+double n_global = 1;    // <---- Usamos una variable global para ver el efecto de: hilos usando una memoria comun.
+
+//void sharedResource(double n) { sleep(8); n_global += n; }
+
+void sharedResource(double x) { mtx.lock(); sleep(8); n_global += x; mtx.unlock(); }
 
 int main() {
 /*
-  delayFunction(2);
-  delayFunction(2);
+  sharedResource(3);
+  sharedResource(2);
 */
-// OJO INVESTIGAR ESTO!! ASI APARECE EN EL LAB DE LA CHICAGO UNIV Y CREO RECORDAR QUE ASI ERA EN LA ENTREVISTA!!!
-  std::thread t1(&delayFunction, 2);
-  std::thread t2(&delayFunction, 2);
+  std::thread t1(sharedResource, 3);    // Pudiera pasar la direccion de la funcion "&delayFunction"!!
+  std::thread t2(sharedResource, 2);
+
   t1.join();
   t2.join();
 
@@ -26,8 +31,8 @@ int main() {
   std::vector<std::thread> threads;
 
   for (double i = 0; i < 2; i++) {
-    threads.push_back(std::thread(delayFunction, i));
-    //delayFunction(i);
+    threads.push_back(std::thread(sharedResource, i));
+    //sharedResource(i);
   }
 
   for (auto &t : threads) {
